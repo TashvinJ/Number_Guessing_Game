de098bf4f2ca1a415bd1d089a5ec7764080587bb
TashvinJ <saptapavank@gmail.com>
2023-09-24 21:52:14 +0530

    test

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -1,1 +1,29 @@
 from time import sleep
+from sys import exit
+
+print("Welcome to numebr guessing game")
+sleep(1)
+
+print("Ok, lets start the game")
+
+def game():
+    number1 = input("Enter a number: ")
+    number2 = input("Enter another number to add both numbers: ")
+    result = int(number1) + int(number2)
+    print(f"The result of your number is: {result}")
+
+if __name__ == "__main__":
+    while True:
+        doplay = input("Do you want to play the game (yes or no): ")
+        if doplay == "yes" or "Yes":
+            game()
+    
+        else:
+            exit()
+    
+        doplayagain = input("Do you want to play the game again (yes or no): ")
+        if doplayagain == "yes" or "Yes":
+            game()
+        
+        continue
+    
